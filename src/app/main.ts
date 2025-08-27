6d9ebc81195dbc5a0f683a6859ca755cfed6d237
Peter Michon <peter.michon@proton.me>
2025-08-27 10:09:44 +0200

    add basic vite setup

---
+++ src/app/main.ts
@@ -0,0 +1,8 @@
+function main() {
+  const pathname = globalThis.location.pathname
+  // console.log(pathname)
+
+  const app = document.getElementById('app')!
+}
+
+export { main }
---
+++ src/tailwind.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: ['./index.html', './index.ts', './app/**/*.{html,js,ts,jsx,tsx}'],
+  theme: {},
+  plugins: [],
+}
+
+export default config
